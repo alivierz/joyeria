68320e1727c2efc46f758e5eb71162af8419ca92
Alivier Zapata <alejandroalivier20@gmail.com>
2022-05-12 15:32:35 -0400

    inicio

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path='/singUp' element={<Register/>}/>
           <Route element={<ProtectedPages/>}>
-            <Route path='/' />
+            <Route path='/' element={<Shop/>} />
             <Route path='/shop' element={<Shop/>}/>
             <Route path='/shop/:id' element={<Product/>} />
             <Route path='/cart' element={<Cart/>}/>
